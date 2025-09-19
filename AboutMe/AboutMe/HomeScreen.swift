e2aa91be80cd506853ca2ba495431bed80f938ed
Mary-Hodges <maryhodges326@gmail.com>
2025-09-18 23:27:52 -0600

    Will style the Home Screen

--- AboutMe/AboutMe/HomeScreen.swift
+++ AboutMe/AboutMe/HomeScreen.swift
@@ -10,44 +10,81 @@ import SwiftUI
 struct HomeScreen: View {
     var body: some View {
         VStack {
-            Text("Mary Hodges")
-                .font(.largeTitle)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding() // Maybe take the out. Need to see how it looks with pictures.
-            
+            ZStack {
+                Rectangle()
+                    .fill(.brown)
+                    .frame(maxWidth: .infinity, maxHeight: 20)
+                    
+                // I want a custom font
+                Text("Mary Hodges")
+                    .font(.largeTitle)
+                    .bold() // I might not want this....
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+            }
             Spacer()
         
             HStack {
                 VStack(spacing: 15) {
-                    NavigationLink("Pets") {
+                    NavigationLink {
                         PetScreen()
+                    } label: {
+                        ImageLayout(
+                            picture: "MyCats",
+                            title: "My Cats"
+                        )
                     }
                     
-                    NavigationLink("Dice") {
+                    NavigationLink {
                         DiceScreen()
+                    } label: {
+                        ImageLayout(
+                            picture: "MyCats",
+                            title: "My Dice"
+                        )
                     }
                 }
                 
                 Spacer()
                 
                 VStack(spacing: 15) {
-                    NavigationLink("Anime") {
+                    NavigationLink {
                         AnimeScreen()
+                    } label: {
+                        ImageLayout(
+                            picture: "MyCats",
+                            title: "Favorite Anime"
+                        )
                     }
                     
-                    NavigationLink("Flowers") {
+                    NavigationLink {
                         FlowersScreen()
+                    } label: {
+                        ImageLayout(
+                            picture: "MyCats",
+                            title: "Favorite Flowers"
+                        )
                     }
                     
-                    NavigationLink("Videogames") {
+                    NavigationLink {
                         VideogamesScreen()
+                    } label: {
+                        ImageLayout(
+                            picture: "MyCats",
+                            title: "Favorite Videogames"
+                        )
                     }
                 }
             }
-            .padding() // Maybe take the out. Need to see how it looks with pictures.
             Spacer()
         }
         .padding()
+        .background {
+            Image(.aboutMeBackground)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+        }
     }
 }
 
--- AboutMe/AboutMe/ViewComponents/ImageLayout.swift
+++ AboutMe/AboutMe/ViewComponents/ImageLayout.swift
@@ -8,20 +8,38 @@
 import SwiftUI
 
 struct ImageLayout: View {
+    
+    var picture: String
+    var title: String
+    
     var body: some View {
-        ZStack {
-            Image(.myCats)
+        ZStack(alignment: .bottom) {
+            Image(picture)
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-            Text("Title")
-                .font(.title)
-                
+                .dynamicTypeSize(.accessibility1)
+            
+            Rectangle()
+                .fill(.clear)
+                .background(
+                    // Find a differnet color to be the gradient. It looks too dark.
+                LinearGradient(
+                    colors:[.black, .clear], startPoint: .bottom, endPoint: .top)
+            )
+            
+            Text(title)
+                .font(.title2)
+                .padding(5)
         }
-        .foregroundStyle(Color.red)
+        .foregroundStyle(Color.brown)
+        .clipShape(
+            RoundedRectangle(cornerRadius: 10)
+        )
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke(.brown, lineWidth: 2))
+//        .shadow(radius: 1)
         
     }
 }
 
 #Preview {
-    ImageLayout()
+    ImageLayout(picture: "MyCats", title: "Title")
 }
